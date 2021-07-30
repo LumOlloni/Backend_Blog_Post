962ba5612b43bbb72f9d2b76b62a297f0a5f9c61
lumolloni@hotmail.com <lum-olloni@hotmail.com>
2021-07-30 12:36:24 +0200

    change message

--- app/routes/hello.ts
+++ app/routes/hello.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 
 export default (fastify: FastifyInstance, opts: Object, next: Function) => {
   fastify.get("/hello", async (req, replay) => {
-    replay.code(200).send({ name: "Harbin" });
+    replay.code(200).send("Hello World");
   });
   next();
 };
